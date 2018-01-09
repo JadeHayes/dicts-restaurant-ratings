6233b3729ee9c49d3b5a4ec21ba2188745a48570
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:30:31 -0800

    Added user restaurant rating input

--- ratings.py
+++ ratings.py
@@ -10,7 +10,34 @@ for line in file_name:
     line = line.rstrip().split(":")
     ratings[line[0]] = line[1]
 
-ratings_lst = ratings.items()
 
-for name, rating in sorted(ratings_lst):
-    print "{} is rated at {}".format(name, rating)
+def print_ratings(ratings):
+    """turns dictionary into list of tuples and prints contents"""
+    print "\n"
+    ratings_lst = ratings.items()
+    for name, rating in sorted(ratings_lst):
+        print "{} is rated at {}".format(name, rating)
+
+# call function to create dictionary with ratings
+# ask user for new restaurant and ratings
+# call function with users input
+# print
+
+
+def user_input():
+    """gets restaurant and rating from user, adds to dictionary, calls print
+    function"""
+
+    user_restaurant = raw_input("What is the name of the restaurant? ").title()
+    user_rating = raw_input("What is your rating for {}? ".format(user_restaurant))
+
+    ratings[user_restaurant] = user_rating
+
+    add_another = raw_input("Would you like to add another restaurant. 'Y' for "
+                            "yes, 'N' for no. ")
+    if add_another.upper() == "Y":
+        user_input()
+
+    return print_ratings(ratings)
+
+user_input()
